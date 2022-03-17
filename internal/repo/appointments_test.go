5c09858e573d571fcc141a8722e5630ba6e370a5
Sam Mahr <samuelmahr@gmail.com>
2022-03-17 02:29:39 -0500

    finished test i forgot about

--- internal/repo/appointments_test.go
+++ internal/repo/appointments_test.go
@@ -119,12 +119,19 @@ func TestAppointmentRepository_CreateAppointment(t *testing.T) {
 
 func TestAppointmentRepository_GetScheduledAppointments(t *testing.T) {
 	type args struct {
+		TrainerID int64
+		StartsAt  time.Time
+		EndsAt    time.Time
+	}
+
+	type fields struct {
 		appointments []models.AppointmentCreateRequest
 	}
 
 	tests := []struct {
 		name    string
 		args    args
+		fields  fields
 		want    models.Appointment
 		wantErr bool
 	}{
@@ -138,37 +145,15 @@ func TestAppointmentRepository_GetScheduledAppointments(t *testing.T) {
 				EndsAt:    time.Date(2022, 03, 17, 12, 30, 0, 0, time.UTC),
 			},
 			args: args{
-				appointments: []models.AppointmentCreateRequest{
-					{
-						TrainerID: 1,
-						UserID:    1,
-						StartsAt:  time.Date(2022, 03, 17, 12, 0, 0, 0, time.UTC),
-						EndsAt:    time.Date(2022, 03, 17, 12, 30, 0, 0, time.UTC),
-					},
-				},
-			},
-		},
-		{
-			name: "error time slot already exists",
-			want: models.Appointment{
-				ID:        1,
 				TrainerID: 1,
-				UserID:    1,
-				StartsAt:  time.Date(2022, 03, 17, 12, 0, 0, 0, time.UTC),
-				EndsAt:    time.Date(2022, 03, 17, 12, 30, 0, 0, time.UTC),
+				StartsAt:  time.Date(2022, 03, 15, 12, 0, 0, 0, time.UTC),
+				EndsAt:    time.Date(2022, 03, 19, 12, 0, 0, 0, time.UTC),
 			},
-			wantErr: true,
-			args: args{
+			fields: fields{
 				appointments: []models.AppointmentCreateRequest{
 					{
 						TrainerID: 1,
 						UserID:    1,
-						StartsAt:  time.Date(2022, 03, 17, 12, 0, 0, 0, time.UTC),
-						EndsAt:    time.Date(2022, 03, 17, 12, 30, 0, 0, time.UTC),
-					},
-					{
-						TrainerID: 2,
-						UserID:    2,
 						StartsAt:  time.Date(2022, 03, 17, 12, 0, 0, 0, time.UTC),
 						EndsAt:    time.Date(2022, 03, 17, 12, 30, 0, 0, time.UTC),
 					},
@@ -184,13 +169,30 @@ func TestAppointmentRepository_GetScheduledAppointments(t *testing.T) {
 				db: DB,
 			}
 
-			for _, appt := range tt.args.appointments {
-				got, err := r.CreateAppointment(context.Background(), appt)
-				fmt.Printf("%#v", got)
+			for _, appt := range tt.fields.appointments {
+				_, err := r.CreateAppointment(context.Background(), appt)
 				if err != nil {
 					t.Fatal(err)
 				}
 			}
+
+			got, err := r.GetScheduledAppointments(context.Background(), tt.args.TrainerID, tt.args.StartsAt, tt.args.EndsAt)
+			if err != nil && tt.wantErr {
+				// I'd really prefer to assert the error otherwise we could have false positive tests
+				return
+			} else if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Len(t, got, 1)
+
+			appt := got[0]
+			assert.Equal(t, tt.want.ID, appt.ID)
+			assert.Equal(t, tt.want.TrainerID, appt.TrainerID)
+			assert.Equal(t, tt.want.UserID, appt.UserID)
+			assert.Equal(t, tt.want.StartsAt, appt.StartsAt)
+			assert.Equal(t, tt.want.EndsAt, appt.EndsAt)
+
 		})
 	}
 }
